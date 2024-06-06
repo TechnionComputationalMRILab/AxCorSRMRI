0cdb2d48fce5d4cf2e283b18bd22e076a2d68627
zemmy <60461456+tcml-bme@users.noreply.github.com>
2024-06-06 13:03:18 +0300

    readme + setup.py corrections

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     readme = f.read()
@@ -14,5 +14,6 @@ setup(
     author='Technion Computational MRI Lab',
     author_email='tcml.bme@gmail.com',
     url='https://github.com/TechnionComputationalMRILab/AxCorSRMRI',
+    packages=find_packages(),
     install_requires=requirements,
 )
